3e11619fcc2395a6020837a76ef5ae363c81a34f
Athlantix <mazigh.sadali@gmail.com>
2021-07-03 22:46:24 +0200

    verif-email

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,19 +3,34 @@ const bcrypt=require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const sha256 = require('sha256');
+const validator = require("email-validator");
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-      .then(hash => {
-        const user = new User({
-          email: sha256.x2(req.body.email),
-          password: hash
-        });
-        
+    try{
+        if(validator.validate(req.body.email)===true){
+
+          bcrypt.hash(req.body.password, 10)
+          .then(
+            hash => {
+              validator.validate(req.body.email);
+              console.log(validator.validate(req.body.email))
+            const user = new User({
+              email: sha256.x2(req.body.email),
+              password: hash
+            });
         user.save()
           .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
           .catch(error => res.status(400).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+        }else{
+          throw 'Invalid';
+        }
+      }catch {
+          res.status(500).json({
+            error: new Error('Invalid request!')
+          });
+        }
+     
   };
 
   exports.login = (req, res, next) => {
